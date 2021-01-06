471867bbdd45ab756ffb01b786ad65074e413bf9
karthicknath <karthick.nath@gmail.com>
2021-01-06 17:50:10 +0530

    VJ Urbanpiper Front End 06.01.2021 5.50 pm

--- src/app/pages/apps/urbanpiper/urbanpiper.component.ts
+++ src/app/pages/apps/urbanpiper/urbanpiper.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core'
 import { NzFormatEmitEvent, NzTreeNodeOptions, NzTreeComponent } from 'ng-zorro-antd/tree'
-
-
-
 
 @Component({
   selector: 'app-urbanpiper',
   templateUrl: './urbanpiper.component.html',
-  styleUrls: ['./urbanpiper.component.scss']
+  styleUrls: ['./urbanpiper.component.scss'],
 })
 export class UrbanpiperComponent implements OnInit {
+  // Serach Filter Icon
+  size = 'large'
+
   @ViewChild('nzTreeComponent') nzTreeComponent: NzTreeComponent
   defaultCheckedKeys = ['10020']
   defaultSelectedKeys = ['10010']
@@ -24,16 +24,33 @@ export class UrbanpiperComponent implements OnInit {
           title: 'Fresh Cream Cakes 1 Kg + (1/2 Kg Free)',
           key: '1001',
           children: [
-            { title: 'Vanilla 1 KG + (1/2 KG Free)', key: '10010', isLeaf: true },
-            { title: ' Black Forest 1 KG + (1/2 KG Free)', key: '10011', isLeaf: true },
+            {
+              title: 'Vanilla 1 KG + (1/2 KG Free)',
+
+              icon: '<fa fa-dot-circle-o />',
+              key: '10010',
+              isLeaf: true,
+            },
+
+            {
+              title: ' Black Forest 1 KG + (1/2 KG Free)',
+              key: '10011',
+              isLeaf: true,
+            },
           ],
         },
-     
       ],
     },
-    
   ]
+  inputValue: string
+  options: string[] = []
 
+  inputValueCustom: string
+  optionsCustom: string[] = []
+
+  onInput(value: string): void {
+    this.options = value ? [value, value + value, value + value + value] : []
+  }
 
   nzClick(event: NzFormatEmitEvent): void {
     console.log(event)
@@ -48,7 +65,7 @@ export class UrbanpiperComponent implements OnInit {
     console.log(keys, this.nzTreeComponent.getSelectedNodeList())
   }
 
-  ngOnInit(): void { }
+  ngOnInit(): void {}
 
   ngAfterViewInit(): void {
     // get node by key: '10011'
@@ -61,5 +78,4 @@ export class UrbanpiperComponent implements OnInit {
       this.nzTreeComponent.getExpandedNodeList(),
     )
   }
-
 }
