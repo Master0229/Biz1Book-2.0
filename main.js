3bd3b69abc8aa44fb6a4b79fc19bf53613002d05
karthicknath <karthick.nath@gmail.com>
2021-01-11 17:56:39 +0530

    Electron 2 nd Push 11.01.2021 05.56 PM

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-const {app, BrowserWindow} = require('electron')
+const { app, BrowserWindow, Menu } = require('electron')
+
 const url = require("url");
 const path = require("path");
 const electron = require('electron');
@@ -9,45 +10,75 @@ const electron = require('electron');
 
 let mainWindow
 
-function createWindow () {
-  mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      nodeIntegration: true,
-      enableRemoteModule: true
-    }
-  })
-
-  mainWindow.loadURL(
-    url.format({
-      pathname: path.join(__dirname, `/dist/index.html`),
-      protocol: "file:",
-      slashes: true
+function createWindow() {
+    mainWindow = new BrowserWindow({
+        // backgroundColor: ' rgb(27, 69, 160)',
+        titleBarStyle: "hiddenInset",
+        width: 800,
+        height: 600,
+        icon: __dirname + `/dist/favicon.png`,
+        webPreferences: {
+            nodeIntegration: true,
+            enableRemoteModule: true
+        }
     })
-  );
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools()
 
-  mainWindow.on('closed', function () {
-    mainWindow = null
-  })
+    mainWindow.loadURL(
+        url.format({
+            pathname: path.join(__dirname, `/dist/index.html`),
+            protocol: "file:",
+            slashes: true
+        })
+    );
+    // Open the DevTools.
+    mainWindow.webContents.openDevTools()
+
+    mainWindow.on('closed', function () {
+        mainWindow = null
+    });
+
+    //   Hide Menu Bar Electron
+    mainWindow.setMenu(null)
+
+    var menu = Menu.buildFromTemplate([
+        {
+            label: 'Biz1Book POS',
+            submenu: [
+                {
+                    label: 'Reload',
+                    click() { redirect(); }
+                },
+                { role: 'toggledevtools' },
+                { role: 'togglefullscreen' },
+                { role: 'close' },
+            ]
+        }
+    ])
+    Menu.setApplicationMenu(menu);
+
 }
+// function toggleFullscreen() {
+//     if (mainWindow.isFullScreen()) {
+//         mainWindow.setFullScreen(false);
+//     } else {
+//         mainWindow.setFullScreen(true);
+//     }
+// }
 
 app.on('ready', createWindow)
 
 app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit()
+    if (process.platform !== 'darwin') app.quit()
 })
 
 app.on('activate', function () {
-  if (mainWindow === null) createWindow()
+    if (mainWindow === null) createWindow()
 })
 
 
 //////////////////////////////////////PRINT/////////////////////////////////////////////////////////////
 global.GetPrinters = function () {
-  return mainWindow.webContents.getPrinters()
+    return mainWindow.webContents.getPrinters()
 }
 // global.scanlan = function () {
 //   return find();
@@ -59,29 +90,29 @@ global.GetPrinters = function () {
 //   return database.app
 // }
 global.Print = function (template, printer) {
-  console.log(printer);
-  const options = {
-    preview: false,                                 // Preview in window or print
-    width: '300px',                                 //  width of content body
-    margin: '0 0 0 0',                              // margin of content body
-    copies: 1,                                      // Number of copies to print
-    printerName: printer,         // printerName: string, check it at webContent.getPrinters()
-    timeOutPerLine: 400,
-    silent: true
-  }
-
-  const data = [
-    {
-      type: 'text',                                       // 'text' | 'barCode' | 'qrCode' | 'image'
-      value: `${template}`,
-      style: ``,
-      css: {}
+    console.log(printer);
+    const options = {
+        preview: false,                                 // Preview in window or print
+        width: '300px',                                 //  width of content body
+        margin: '0 0 0 0',                              // margin of content body
+        copies: 1,                                      // Number of copies to print
+        printerName: printer,         // printerName: string, check it at webContent.getPrinters()
+        timeOutPerLine: 400,
+        silent: true
     }
-  ]
 
-  PosPrinter.print(data, options)
-    .then(() => { })
-    .catch((error) => {
-      console.error(error);
-    });
+    const data = [
+        {
+            type: 'text',                                       // 'text' | 'barCode' | 'qrCode' | 'image'
+            value: `${template}`,
+            style: ``,
+            css: {}
+        }
+    ]
+
+    PosPrinter.print(data, options)
+        .then(() => { })
+        .catch((error) => {
+            console.error(error);
+        });
 }
